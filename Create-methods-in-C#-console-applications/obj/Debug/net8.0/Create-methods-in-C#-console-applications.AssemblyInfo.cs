b92ea4aa31829c1780a55591a3ae0eef1a6fcefb
Faiza <faraz_ul_islam@yahoo.com>
2024-10-29 19:08:09 +0000

    done

--- Create-methods-in-C#-console-applications/obj/Debug/net8.0/Create-methods-in-C#-console-applications.AssemblyInfo.cs
+++ Create-methods-in-C#-console-applications/obj/Debug/net8.0/Create-methods-in-C#-console-applications.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Create-methods-in-C#-console-applications")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f824debf8405a5e9763061f4fdf68e6a0dae04bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d490310e319d1bdcc15c81731c34ef92f59599")]
 [assembly: System.Reflection.AssemblyProductAttribute("Create-methods-in-C#-console-applications")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Create-methods-in-C#-console-applications")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
